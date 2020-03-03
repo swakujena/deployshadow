92a25ba6a1f3ef6c980df59e1cf057f8d1297d15
Swaroop Jena <swakujena@gmail.com>
2020-03-03 15:42:28 +0100

    Updates

---
+++ precache-manifest.c9c82c50d9a83c1a8700079438f66d19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f36f44f70a411f7802067cd110f5ba8",
+    "url": "/http://swakujena.github.io/deployshadow/index.html"
+  },
+  {
+    "revision": "1667ae403c3a2a6a0962",
+    "url": "/http://swakujena.github.io/deployshadow/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ffbb0d853be924e60cf1",
+    "url": "/http://swakujena.github.io/deployshadow/static/css/main.69b3b498.chunk.css"
+  },
+  {
+    "revision": "1667ae403c3a2a6a0962",
+    "url": "/http://swakujena.github.io/deployshadow/static/js/2.3376f58f.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/http://swakujena.github.io/deployshadow/static/js/2.3376f58f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffbb0d853be924e60cf1",
+    "url": "/http://swakujena.github.io/deployshadow/static/js/main.fa753fbf.chunk.js"
+  },
+  {
+    "revision": "6a59259081ac22cfeffa",
+    "url": "/http://swakujena.github.io/deployshadow/static/js/runtime-main.65996bc0.js"
+  }
+]);
